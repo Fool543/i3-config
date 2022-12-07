68077e44f60244c072cf0046e7569f4c6ff85f49
Fool543 <120022950+Fool543@users.noreply.github.com>
2022-12-07 20:47:40 +0530

    Update and rename install.sh to start.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3-wm dmenu xorg-xbacklight i3status ttf-roboto alacritty firefox nautilus vim git rofi picom -y \
-sudo rm .xinitrc
-sudo cp .xinitrc ~/
---
+++ start.sh
@@ -0,0 +1,11 @@
+sudo rm .xinitrc &&
+sudo cp .xinitrc &&
+sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3-gaps dmenu xorg-xbacklight i3status ttf-roboto alacritty firefox nautilus vim git rofi picom -y &&
+git clone https://aur.archlinux.org/yay.git &&
+cd yay &&
+makepkg -si &&
+yay -S bumblebee-status python-pulsectl &&
+rm ~/.config/i3/config &&
+cp config ~/.config/i3/config &&
+startx i3
+
