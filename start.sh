f11c64490e23690b2ee6d23dbb18ba5293d555dc
Fool543 <120022950+Fool543@users.noreply.github.com>
2022-12-07 23:22:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 sudo systemctl enable --now NetworkManager &&
-sudo rm .xinitrc &&
-sudo cp .xinitrc &&
-sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto alacritty firefox nautilus vim git rofi picom -y &&
+sudo rm $HOME/.xinitrc &&
+sudo cp .xinitrc $HOME/.xinitrc &&
+sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto alacritty firefox nautilus vim git rofi picom &&
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
 makepkg -si &&
 yay -S bumblebee-status python-pulsectl &&
 rm ~/.config/i3/config &&
+cd $HOME/i3-config &&
 cp config ~/.config/i3/config &&
 mkdir ~/.config/picom &&
 cp picom.conf ~/.config/picom/picom.conf &&
