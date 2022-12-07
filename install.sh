75d2dcadabc897c5197f46965bed7df2a32f2999
Fool543 <120022950+Fool543@users.noreply.github.com>
2022-12-07 11:49:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo pacman -S xorg-server xorg-apps xorg-xinit i3-wm dmenu xbacklight i3status ttf-roboto alacritty firefox Nautilus vim
+sudo pacman -S xorg-server xorg-apps xorg-xinit i3-wm dmenu xbacklight i3status ttf-roboto alacritty firefox Nautilus vim git rofi picom 
