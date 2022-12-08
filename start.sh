218da24fb663c7c4ad6cab7f8e8b353789222dcd
Fool543 <120022950+Fool543@users.noreply.github.com>
2022-12-08 08:09:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 sudo systemctl enable --now NetworkManager &&
-sudo rm $HOME/.xinitrc &&
-sudo cp .xinitrc $HOME/.xinitrc &&
-sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto alacritty firefox nautilus vim git rofi picom &&
+sudo cp .xinitrc $HOME/ &&
+sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto firefox nautilus vim git rofi picom lxappearance fish &&
+# sudo pacman -S alacritty &&
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
 makepkg -si &&
 yay -S bumblebee-status python-pulsectl &&
-rm ~/.config/i3/config &&
 cd $HOME/i3-config &&
-cp config ~/.config/i3/config &&
+cp config ~/.config/i3/ &&
 mkdir ~/.config/picom &&
-cp picom.conf ~/.config/picom/picom.conf &&
-mkdir ~/.config/alacritty &&
-cp alacritty.yml ~/.config/alacritty/alacritty.yml
+cp picom.conf ~/.config/picom/ &&
+# mkdir ~/.config/alacritty &&
+# cp alacritty.yml ~/.config/alacritty/ &&
 startx i3
 
