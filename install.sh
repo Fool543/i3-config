d3db48859ca72e75b7df1abfbef58c3c43c5d58d
Fool543 <120022950+Fool543@users.noreply.github.com>
2022-12-08 19:50:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto firefox nautilus vim git rofi picom lxappearance alacritty &&
+sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3-gaps i3-block xorg-xbacklight i3status ttf-roboto firefox nautilus vim git rofi picom lxappearance alacritty &&
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
 makepkg -si &&
