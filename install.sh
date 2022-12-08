3fdd7c2dbb1e77e3708ece74fa23d5e4f71c3f96
Fool543 <120022950+Fool543@users.noreply.github.com>
2022-12-08 19:50:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto firefox nautilus vim git rofi picom lxappearance fish &&
-# sudo pacman -S alacritty &&
+sudo pacman -S --needed xorg-server xorg-apps xorg-xinit i3 xorg-xbacklight i3status ttf-roboto firefox nautilus vim git rofi picom lxappearance alacritty &&
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
 makepkg -si &&
-yay -S bumblebee-status python-pulsectl 
+yay -S bumblebee-status python-pulsectl qt5-styleplugins
